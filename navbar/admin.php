8a23088282f70760bff3c1df3c6f0005476f2b30
Chip <chip4@live.com>
2012-12-04 23:54:15 +0000

    added admin report

--- navbar/admin.php
+++ navbar/admin.php
@@ -1,4 +1,4 @@
 <ul class="nav">
-  <li id="view_admin_report"><a href="#">View Administrative Report</a></li>
+  <li id="view_admin_report"><a href="admin_report.php">View Administrative Report</a></li>
   <li id="add_new_course"><a href="#">Add New Course</a></li>
 </ul>
