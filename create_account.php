cb6918ae3f66d77b8d7c61824d6ab1e71ece97cd
Chip <chip4@live.com>
2012-12-04 02:55:22 +0000

    create_account out of debug mode and redirecting to home page

--- create_account.php
+++ create_account.php
@@ -1,5 +1,8 @@
 <?php
 include('database.php');
+
+$debug = 0;
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
@@ -14,13 +17,16 @@ switch ($method) {
 
 
 function handleForm(){
-	echo '<pre>';
-	var_dump($_POST);
+	if($debug){
+		echo '<pre>';
+		var_dump($_POST);
+	}
 
 	$userType = strtolower($_POST['user_type']);
 
 	$users_query = "INSERT INTO users (username,password,type) VALUES ('".$_POST['username']."','".$_POST['password']."','".$userType."');";
-	echo "users_query: ".$users_query."\n";
+	if($debug)
+		echo "users_query: ".$users_query."\n";
 	if(!mysql_query($users_query))
 		die('Error: '.mysql_error());
 
@@ -31,7 +37,8 @@ function handleForm(){
 			$_POST['permanent_address_line1']." ".$_POST['permanent_address_line2']." ".$_POST['permanent_address_line3']."','".
 			$_POST['current_address_line1']." ".$_POST['current_address_line2']." ".$_POST['current_address_line3']."','".
 			$_POST['gender']."','".$_POST['phone_number']."');";
-	echo "reg_users_query: ".$reg_users_query."\n";
+	if($debug)
+		echo "reg_users_query: ".$reg_users_query."\n";
 	if(!mysql_query($reg_users_query))
 		die('Error: '.mysql_error());
 
@@ -40,28 +47,32 @@ function handleForm(){
 	if($userType == "students"){
 		$student_query = "INSERT INTO students (user_id,department_id,degree) VALUES (".$user_id.",".$_POST['student_degree'].",'".$_POST['student_degree_type']."');";
 
-		echo "student_query: ".$student_query."\n";
+		if($debug)
+			echo "student_query: ".$student_query."\n";
 		if(!mysql_query($student_query))
 			die('Error: '.mysql_error());
 
 		//school history
 		for($i=1;$i<4;$i++){
-			if(isset($_POST['previous_education'.$i.'_name'])){
-				$prefix = 'previous_education'.$i;
+			$prefix = 'previous_education'.$i;
+			if(isset($_POST[$prefix.'_name']) && $_POST[$prefix.'_name'] != ""){
 				$history_query = "INSERT INTO school_histories (student_id,grad_year,name,gpa,major,degree) VALUES (".$user_id.",'".$_POST[$prefix.'_year']."','".$_POST[$prefix.'_name']."','".$_POST[$prefix.'_gpa']."','".$_POST[$prefix.'_major']."','".$_POST[$prefix.'_degree']."');";
-				echo "history_query: ".$history_query."\n";
+				if($debug)
+					echo "history_query: ".$history_query."\n";
 				if(!mysql_query($history_query))
 					die('Error: '.mysql_error());
 			}
 		}
 	}else{ //FACULTY QUERY
 		$faculty_query = "INSERT INTO faculty (user_id,department_id,position) VALUES (".$user_id.",".$_POST['faculty_department'].",'".$_POST['faculty_position']."');";
-		echo "faculty_query: ".$faculty_query."\n";
+		if($debug)
+			echo "faculty_query: ".$faculty_query."\n";
 		if(!mysql_query($faculty_query))
 			die('Error: '.mysql_error());
 
 		$faculty_query = "INSERT INTO `faculty-sections` (faculty_id,section_id) VALUES (".$user_id.",".$_POST['faculty_course_section'].");";
-		echo "faculty_sections_query: ".$faculty_query."\n";
+		if($debug)
+			echo "faculty_sections_query: ".$faculty_query."\n";
 		if(!mysql_query($faculty_query))
 			die('Error: '.mysql_error());
 
@@ -71,14 +82,19 @@ function handleForm(){
 			foreach($research_interests as $interest){
 				$interest = trim($interest);
 				$interest_query = "INSERT INTO research_interests (faculty_id,name) VALUES (".$user_id.",'".$interest."');";
-				echo "interest_query: ".$interest_query."\n";
+				if($debug)
+					echo "interest_query: ".$interest_query."\n";
 				if(!mysql_query($interest_query))
 					die('Error: '.mysql_error());
 			}
 		}
 	}
 
-	echo '</pre>';
+	if($debug)
+		echo '</pre>';
 	//GOTO index.php
+	if(!$debug)
+		header("Location: index.php");
+	exit;
 }
 ?>
