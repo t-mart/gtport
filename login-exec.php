093cb619330fd302c28ef0341bc3804f42ae73d6
Chip <chip4@live.com>
2012-12-04 17:56:24 +0000

    username displayed in top right corner and navbars working

--- login-exec.php
+++ login-exec.php
@@ -1,4 +1,6 @@
 <?php
+	$debug = 0;
+
   //Start session
   session_start();
   
@@ -12,7 +14,8 @@
   $errflag = false;
   
   //Sanitize the POST values
-var_dump($_POST);
+	if($debug)
+		var_dump($_POST);
   $username = clean($_POST['username']);
   $password = clean($_POST['password']);
   
@@ -29,28 +32,29 @@ var_dump($_POST);
   //If there are input validations, redirect back to the login form
   if($errflag) {
     $_SESSION['error_alerts'] = $errmsg_arr;
-    session_write_close();
+    //session_write_close();
     header("location: index.php");
     exit();
   }
   
   //Create query
-  $query= sprintf("SELECT id, type FROM users WHERE username='%s' and password='%s'", $username, $password);
+  $query= sprintf("SELECT * FROM users WHERE username='%s' and password='%s'", $username, $password);
   $result=mysql_query($query);
   
   //Check whether the query was successful or not
   if($result) {
     if(mysql_num_rows($result) == 1) {
       //Login Successful
-      session_regenerate_id();
+      //session_regenerate_id();
       $user = mysql_fetch_assoc($result);
       $_SESSION['user_id'] = $user['id'];
       $_SESSION['user_type'] = $user['type'];
+      $_SESSION['user_username'] = $user['username'];
 
       $successmsg_arr[] = 'You have logged in succesfully';
       $_SESSION['success_alerts'] = $successmsg_arr;
 
-      session_write_close();
+      //session_write_close();
 
       //change this to location to redirect the user elsewhere on successful login
       header("location: index.php");
@@ -60,7 +64,7 @@ var_dump($_POST);
       $errmsg_arr[] = 'Username or password is invalid';
       $_SESSION['error_alerts'] = $errmsg_arr;
 
-      session_write_close();
+      //session_write_close();
 
       header("location: index.php");
       exit();
--- navbar/student.php
+++ navbar/student.php
@@ -9,11 +9,10 @@
   <li id="update_personal_information"><a href="#">Update Personal Information</a></li>
   <li id="find_tutors"><a href="#about">Find Tutor</a></li>
   <?php
-  if(isset($_SESSION['user_type'])){
-    $result = mysql_query("SELECT * FROM tutors WHERE student_id=".$_SESSION['user_type'].";");
-    if($row = mysql_fetch_array($result))
-      echo '    <li id="tutor_logbook"><a href="#contact">Tutor Logbook</a></li>';
-  }
+	$result = mysql_query("SELECT * FROM tutors WHERE student_id=".$_SESSION['user_id'].";");
+	
+	if(mysql_num_rows($result)>0)
+		echo '    <li id="tutor_logbook"><a href="#contact">Tutor Logbook</a></li>';
   ?>
   <li id="grade_report"><a href="#contact">View Grading Pattern</a></li>
 </ul>
