3f0081e04d4d92410652843960028027baf5c3a5
Chip <chip4@live.com>
2012-12-05 00:16:39 +0000

    removed assign grades option as we dont have to implement it

--- navbar/faculty.php
+++ navbar/faculty.php
@@ -1,5 +1,4 @@
 <ul class="nav">
-  <li id="assign_grades"><a href="#">Assign Grades</a></li>
   <li id="update_personal_information"><a href="#">Update Personal Information</a></li>
   <li id="assign_tutors"><a href="#">Assign Tutors</a></li>
   <li id="view_student_performance"><a href="#">View Student Performance</a></li>
