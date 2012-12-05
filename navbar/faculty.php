aee2f5bc420a5a55bcee268e79cad6299e49a2b6
Chip <chip4@live.com>
2012-12-05 01:10:40 +0000

    added faculty_report, not functioning properly

--- navbar/faculty.php
+++ navbar/faculty.php
@@ -1,5 +1,5 @@
 <ul class="nav">
   <li id="update_personal_information"><a href="#">Update Personal Information</a></li>
   <li id="assign_tutors"><a href="#">Assign Tutors</a></li>
-  <li id="view_student_performance"><a href="#">View Student Performance</a></li>
+  <li id="view_faculty_report"><a href="faculty_report.php">View Student Performance</a></li>
 </ul>
